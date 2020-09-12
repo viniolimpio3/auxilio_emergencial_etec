6d9b2c02f639721e3351158b1fbd7431900390d0
viniOlimpio <viniolimpio3@gmail.com>
2020-09-12 18:37:25 -0300

    env example

---
+++ env_example.php
@@ -0,0 +1,13 @@
+<?php
+
+if(!defined('DB')) define('DB', 'DATABASE_NAME');
+if(!defined('DB_HOST')) define('DB_HOST', 'HOST');
+if(!defined('DB_USER')) define('DB_USER', 'USER_DB');
+if(!defined('DB_PASS')) define('DB_PASS', 'PASS_DB');
+if(!defined('USER_MAIL')) define('USER_MAIL', 'USER_MAIL');
+if(!defined('USER_MAIL_NAME')) define('USER_MAIL_NAME', 'USER_MAIL_NAME');
+if(!defined('PASS_MAIL')) define('PASS_MAIL','SENHA_EMAIL' );
+if(!defined('SMTP_SERVER')) define('SMTP_SERVER', 'SERVER_SMTP');
+if(!defined('SMTP_PORT')) define('SMTP_PORT', 'PORT_SMPT');
+if(!defined('BASE_URL')) define('BASE_URL','http://YOUR_BASE_URL');
+?>
